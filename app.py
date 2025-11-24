a875b3225b3cd7277c516b478691d28dd5bee832
User <user@example.com>
2025-11-24 13:10:52 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a-b
+
+
+def mul(a, b):
+    return a*b
+
+
+if __name__ == "__main__":
+    result1 = add(4, 5)
+    result2 = sub(4, 1)
+    result3 = mul(3, 7)
+    print(result1, result2, result3)
